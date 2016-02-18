125e41a223fac0ef50d4fdc2adc36b66f17c8040
atang020 <atang1994@gmail.com>
2016-02-18 17:43:48 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(likeClicked);
+};
+
+function likeClicked(e){
+	ga("send", "event", 'like', 'click');
 }
